87324befa04c62854e7ca445f7209ffd415f0362
Johan Lund <johan.h.lund@gmail.com>
2021-05-30 16:29:47 +0200

    Test added to exclude false positives when checking file hash.
    - tests refactored to increase readability and remove duplicate code.
    - test_hash_check_doesnt_match() implemented.

--- tests/test_filehash.py
+++ tests/test_filehash.py
@@ -5,30 +5,37 @@ import json
 from delivery.manifest.filehash import file_hash_check
 
 
-@pytest.mark.parametrize('filename, expected_hash, hash_method, json_data',
-                         [('SHA256.json',
-                           'sha256:4c12882f1ce34f1f0aa5d2f6a902170d35cef128b8eecee5867d5750f5ab5e63',
-                           'sha256',
-                           {"my_hash": 'is always matching'}),
-                          ('SHA1.json',
-                           'sha1:c922c69a3ad8c693b7dcc1e4bf75dbffc6074782',
-                           'sha1',
-                           {"my_hash": 'is always matching'}),
-                          ('MD5.json',
-                           'sha1:545112744d394dd2b7e52b4f4dc717ed',
-                           'md5',
-                           {"my_hash": 'is always matching'})
-                          ])
-def test_file_hash_check_matches(tmpdir, filename, expected_hash, hash_method, json_data):
-    """
-    create dummy file with a known hash in a temporary directory and provide checksum
-    :return:
-    """
-    _file_name = Path(filename)
-    _expected_hash = expected_hash
-    _hash_method = hash_method
-    _json_data = json_data
+@pytest.fixture()
+def hash_file(tmpdir):
+    _file_name = Path('hashed_file.json')
+    _json_data = {"my_hash": 'is always matching'}
     _path = Path(tmpdir).joinpath(_file_name)
-    with open(_path, 'w') as file_pointer:
-        json.dump(_json_data, file_pointer, indent=3)
-    assert file_hash_check(file_name=_path, hash_string=_expected_hash, hash_method=_hash_method)
+    with open(_path, 'w') as fp:
+        json.dump(_json_data, fp, indent=3)
+    yield _path
+
+
+@pytest.mark.parametrize('expected_hash, hash_method',
+                         [('sha256:4c12882f1ce34f1f0aa5d2f6a902170d35cef128b8eecee5867d5750f5ab5e63',
+                           'sha256'),
+                          ('sha1:c922c69a3ad8c693b7dcc1e4bf75dbffc6074782',
+                           'sha1'),
+                          ('sha1:545112744d394dd2b7e52b4f4dc717ed',
+                           'md5')
+                          ])
+class TestHash:
+
+    def test_file_hash_check_matches(self, hash_file, expected_hash, hash_method):
+        """
+        Check dummy file with a known hash with generated checksum
+        :return:
+        """
+        assert file_hash_check(file_name=hash_file, hash_string=expected_hash, hash_method=hash_method)
+
+    def test_file_hash_check_doesnt_match(self, hash_file, expected_hash, hash_method):
+        """
+        copy a well known dummy file into temporary directory and provide checksum
+        :return:
+        """
+        _invalid = str(expected_hash).replace('f', 'c')
+        assert not file_hash_check(file_name=hash_file, hash_string=_invalid, hash_method=hash_method)
