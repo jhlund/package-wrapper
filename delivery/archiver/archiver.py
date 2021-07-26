f169d9ec9f92fedaa8f568d95bf4796ad364d508
Johan Lund <johan.h.lund@gmail.com>
2021-07-26 21:54:50 +0200

    feat: archiver added to compress and decompress folders
    To package the collections of artifacts we need a tool to both
    package them, and unpack them in a controlled manner. This class
    introduced some simple basic method wrapping the zipfile and
    tarfile from standard python into a more usable shape.

---
+++ delivery/archiver/archiver.py
@@ -0,0 +1,64 @@
+import tarfile
+import zipfile
+from pathlib import Path
+
+
+class ArchiveE(Exception):
+    def __init__(self, msg):
+        super(ArchiveE, self).__init__()
+        self.msg = msg
+
+
+class Archive:
+    def __init__(self, file_name: Path):
+        self.archive = file_name
+
+    @staticmethod
+    def _validate(file_path: Path):
+        if not file_path.exists():
+            raise ArchiveE("file: %s not found" % file_path)
+        if file_path.name.endswith("tar.gz") or file_path.name.endswith("tar") or file_path.name.endswith("zip"):
+            if not tarfile.is_tarfile(file_path):
+                raise ArchiveE("file %s is not a zip or TAR ball" % file_path)
+
+    def extract(self, out_dir: Path):
+        Archive._validate(self.archive)
+
+        if self.archive.name.endswith("tar.gz"):
+            with tarfile.open(self.archive, "r:gz") as tar_ref:
+                tar_ref.extractall(out_dir)
+        elif self.archive.name.endswith("tar"):
+            with tarfile.open(self.archive, "r:") as tar_ref:
+                tar_ref.extractall(out_dir)
+        elif self.archive.name.endswith("zip"):
+            with zipfile.ZipFile(self.archive, "r") as zip_ref:
+                zip_ref.extractall(out_dir)
+        else:
+            raise ArchiveE("unsupported file format")
+
+    def compress(self, dir_path: Path, force_relative=True):
+        if not dir_path.is_dir():
+            raise ArchiveE("path: %s is not a directory" % dir_path)
+
+        # handle TAR, TGZ and ZIP
+        if self.archive.name.endswith("tar.gz"):
+            with tarfile.open(self.archive, "w:gz") as tar_ref:
+                tar_ref.add(dir_path, recursive=True)
+        elif self.archive.name.endswith("tar"):
+            with tarfile.open(self.archive, "w") as tar_ref:
+                tar_ref.add(dir_path, recursive=True)
+        elif self.archive.name.endswith("zip"):
+            with zipfile.ZipFile(
+                    self.archive,
+                    mode="w",
+                    compression=zipfile.ZIP_DEFLATED
+            ) as zip_ref:
+                files = [_file for _file in dir_path.glob("**/*") if dir_path.joinpath(_file).is_file()]
+                for file in files:
+                    _abs = file.absolute()
+                    if force_relative:
+                        _rel = _abs.relative_to(dir_path)
+                        zip_ref.write(_abs, _rel)
+                    else:
+                        zip_ref.write(_abs)
+                zip_ref.close()
