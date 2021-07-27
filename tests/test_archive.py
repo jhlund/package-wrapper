89ed262706c4bb267ea0ea37fb1910a4e40dff1f
Johan Lund <johan.lund@metr.systems>
2021-07-27 18:01:44 +0200

    Tests: Tests for the archive added

---
+++ tests/test_archive.py
@@ -0,0 +1,28 @@
+import pytest
+import json
+import os
+from pathlib import Path
+from typing import List
+
+from delivery.archiver.archiver import Archive
+
+@pytest.fixture()
+def generate_files(tmpdir):
+    file_list = ["first/test11", "first/test12", 
+                "second/test21", "second/test22",
+                "first/first_lvl2/test111"]
+    base_dir = Path(tmpdir).joinpath(Path("base"))           
+    file_list = [base_dir.joinpath(Path(_file)) for _file in file_list]
+    for file in file_list:
+        file.parent.mkdir(parents=True, exist_ok=True)
+        with open(file, "w") as file_pointer:
+            file_pointer.write(str(os.urandom(1024)))
+    yield base_dir
+
+
+class Test_archive:
+    def test_add_folder(self, generate_files, tmpdir):
+        archive_path = Path(tmpdir).joinpath(Path("test.zip"))
+        archive = Archive(file_name=archive_path)
+        dir_path = generate_files
+        archive.compress(dir_path=dir_path)
